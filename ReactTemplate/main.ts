8fb870c2d00b30885ab25922ea4991176252cd36
sato-to <sato.one0722@gmail.com>
2022-07-27 17:45:45 +0900

    Refactor

--- ReactTemplate/main.ts
+++ ReactTemplate/main.ts
@@ -1,46 +1,7 @@
-import { fromFileUrl, parse, resolve } from "./deps.ts";
+import { fromFileUrl, resolve } from "./deps.ts";
+import { parseArgs, replaceTemplate } from "./utils.ts";
 
-const args = parse(Deno.args);
-const type: "hook" | "fc" = (() => {
-  const type = args.t || args.type;
-  switch (type) {
-    case "hook":
-    case "h":
-      return "hook";
-    case "fc":
-    case "f":
-      return "fc";
-    default:
-      return "fc";
-  }
-})();
-const capitalizeFirst = (text: string) => {
-  const first = text.slice(0, 1);
-  return text.replace(first, first.toUpperCase());
-};
-const replace = (
-  { text, type, name }: {
-    text: string;
-    type: "hook" | "fc";
-    name: string;
-  },
-): string => {
-  const keyword = (() => {
-    if (type === "fc") {
-      return name;
-    }
-
-    if (name.startsWith("use")) {
-      return name.slice(3);
-    }
-    return name;
-  })();
-
-  return text.replaceAll("Sample", capitalizeFirst(keyword)).replaceAll(
-    "// @ts-ignore\n",
-    "",
-  ).replaceAll(";", "");
-};
+const { type, name, path } = parseArgs(Deno.args);
 
 const template = (() => {
   switch (type) {
@@ -48,39 +9,20 @@ const template = (() => {
       return "./templates/hook.tsx";
     case "fc":
       return "./templates/fc.tsx";
+    default:
+      return "./templates/fc.tsx";
   }
 })();
-const name = args.n || args.name || args._[0];
-const newFilePath = (() => {
-  const path = (() => {
-    const path = args.p || args.path || "./";
+const newFilePath = `${path}${name}.tsx`;
 
-    if (path.slice(-1) === "/") {
-      return path;
-    }
-    return path + "/";
-  })();
-
-  const fileName = (() => {
-    if (type === "fc") {
-      return name;
-    }
-    if (name.startsWith("use")) {
-      return name;
-    }
-    return `use${capitalizeFirst(name)}`;
-  })();
-
-  return `${path}${fileName}.tsx`;
-})();
-
+//テンプレートファイルの読み込み
 const text = await Deno.readTextFile(
   fromFileUrl(new URL(template, import.meta.url)),
 );
-
+//テンプレート置換＆ファイル作成
 await Deno.writeTextFile(
   newFilePath,
-  replace({ text, type, name }),
+  replaceTemplate({ templateText: text, type, fileName: name }),
 );
 
 console.log(`created: ${resolve(Deno.cwd(), newFilePath)}`);
---
+++ ReactTemplate/utils.ts
@@ -0,0 +1,84 @@
+import { parse } from "./deps.ts";
+
+export type FileType = "fc" | "hook";
+
+const parseArgs = (
+  _args: string[],
+): { type: FileType; name: string; path: string } => {
+  const args = parse(_args);
+
+  const type: FileType = (() => {
+    const type = args.t || args.type;
+    switch (type) {
+      case "hook":
+      case "h":
+        return "hook";
+      case "fc":
+      case "f":
+        return "fc";
+      default:
+        return "fc";
+    }
+  })();
+
+  const path = (() => {
+    const path = args.p || args.path || "./";
+
+    if (path.slice(-1) === "/") {
+      return path;
+    }
+    return path + "/";
+  })();
+
+  const name = (() => {
+    const name = args.n || args.name || args._[0];
+
+    if (type === "fc") {
+      return name;
+    }
+    //hookの場合はusexxxの形式にする
+    if (name.startsWith("use")) {
+      return name;
+    }
+    return `use${capitalizeFirst(name)}`;
+  })();
+
+  return {
+    name,
+    type,
+    path,
+  };
+};
+
+// 1文字目を大文字に変換
+const capitalizeFirst = (text: string) => {
+  const first = text.slice(0, 1);
+  return text.replace(first, first.toUpperCase());
+};
+
+// templateの中身をファイル名に合わせて置換する
+const replaceTemplate = (
+  { templateText, type, fileName }: {
+    templateText: string;
+    type: FileType;
+    fileName: string;
+  },
+): string => {
+  const keyword = (() => {
+    if (type === "fc") {
+      return fileName;
+    }
+
+    if (fileName.startsWith("use")) {
+      return fileName.slice(3);
+    }
+    return fileName;
+  })();
+
+  return templateText.replaceAll("Sample", capitalizeFirst(keyword)).replaceAll(
+    "// @ts-ignore\n",
+    "",
+  ).replaceAll(";", "");
+};
+
+export { capitalizeFirst, parseArgs, replaceTemplate };
